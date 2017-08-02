de750f302d276d3210aef2e893ff3bee3440e576
SergeiKaptelin <serhii.kaptelin@gmail.com>
2017-08-02 18:14:49 +0300

    added rspec tests for session#callback

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,6 +29,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include(Shoulda::Matchers::ActiveModel, type: :model)
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
