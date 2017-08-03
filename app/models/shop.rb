4ce1590289561dffcb91d04580ab05b2136bd485
Linch <andrei95-1995@mail.ru>
2017-08-03 16:45:00 +0300

    added serializer and SERVICE_SETTINGS for Shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,10 +1,34 @@
 class Shop < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  SERVICE_SETTINGS = {
+    triggers: {
+      uri: {
+        allowed_types: %w(Does Doesn't),
+        allowed_matching_types: ['match', 'contain', 'begin with', 'end with']
+      }
+    }
+  }
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
   include ShopifyApp::Shop
   include ShopifyApp::SessionStorage
+
+
+  def self.service_settings(key = nil, hash=SERVICE_SETTINGS)
+    return SERVICE_SETTINGS if key == nil
+
+    found = nil
+
+    if hash.respond_to?(:key?) && hash.key?(key)
+      return hash[key]
+    elsif hash.is_a? Enumerable
+      hash.find { |*a| found = service_settings(key, a.last) }
+      return found
+    end
+  end
 
   def activate_session
     session = ShopifyAPI::Session.new(shopify_domain, shopify_token)
