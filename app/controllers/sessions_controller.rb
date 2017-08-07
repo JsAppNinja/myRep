af55e67fdff30936e9ae573b296105b6c7512baf
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2017-08-07 17:32:24 +0300

    refresh shopify token on each sign in/up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,20 +13,20 @@ class SessionsController < ApplicationController
     # create or sign in shop
     account = Shop.find_by(shopify_domain: shop_name) || Shop.new(shopify_domain: shop_name)
 
+    account.shopify_token = response['credentials']['token']
+    account.activate_session
+    shop = ShopifyAPI::Shop.current
+    account.email = shop.email
+
     # sign up
     if account.id.blank?
-      account.shopify_token = response['credentials']['token']
-      account.activate_session
-
-      shop = ShopifyAPI::Shop.current
-      account.email = shop.email
-
       password = SecureRandom.hex(10)
       account.password_confirmation = password
       account.password = password
       account.save!
       EventService.new_shop_registered(account)
     else
+      account.save!
       EventService.new_sign_in(account)
     end
 
