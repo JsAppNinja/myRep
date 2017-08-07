0826a639910b8c9016d951c58eac52d8801bc0dd
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2017-08-07 17:19:57 +0300

    fixed script tag service

--- app/services/script_tag_service.rb
+++ app/services/script_tag_service.rb
@@ -2,13 +2,11 @@ class ScriptTagService
 
   def self.insert_script(shop)
     shop.activate_session
-    script_tag = ShopifyAPI::ScriptTag.create(
-      script_tag: { event: 'onload', src: "#{ENV['HOST']}/scripts/js" }
-    )
-    # remove script tag if we cannot save information about it
-    if !shop.update_columns(shopify_script_tag_id: script_tag.id)
-      script_tag.destroy
-      # TODO: notify about this issue
+    script_tag = ShopifyAPI::ScriptTag.create(script_tag: { event: 'onload', src: "#{ENV['SCRIPTS_HOST']}/scripts/js" })
+    if tag.id.present?
+      shop.update_columns(shopify_script_tag_id: script_tag.id)
+    else
+      Rollbar.error(StandardError.new(errors: script_tag.errors.full_messages))
     end
   end
 end
