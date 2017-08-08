8c91fa9d726436ca70c96fc1e6e81765097f2aa2
Linch <andrei95-1995@mail.ru>
2017-08-08 15:07:57 +0300

    moved "skip_before_action   :verify_authenticity_token" to PopupConfigsController

--- app/controllers/api/internal/v1/popup_configs_controller.rb
+++ app/controllers/api/internal/v1/popup_configs_controller.rb
@@ -2,6 +2,7 @@ module Api
   module Internal
     module V1
       class PopupConfigsController< ApplicationController
+        skip_before_action :verify_authenticity_token
 
         def show
           render json: current_shop
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  skip_before_action   :verify_authenticity_token
   before_action        :authenticate_shop
 
 
