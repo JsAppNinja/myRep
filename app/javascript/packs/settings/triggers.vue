24c585e896b97c9d0329018d718c9a61384c10fa
Linch <andrei95-1995@mail.ru>
2017-08-09 13:25:22 +0300

    added disabled statement for reset cookies button

--- app/javascript/packs/settings/triggers.vue
+++ app/javascript/packs/settings/triggers.vue
@@ -133,7 +133,7 @@
 
               <v-flex sm12>
                 <p>Show it again to everyone no matter how many days have passed.</p>
-                <v-btn info small class="ma-0" @click="resetCookies()">Reset cookies for all users</v-btn>
+                <v-btn info small class="ma-0" light v-bind:disabled="disable_cookies_reset" @click="resetCookies()">Reset cookies for all users</v-btn>
               </v-flex>
             </v-layout>
           </v-card-title>
@@ -226,6 +226,12 @@
 
 <script>
   export default {
+    data: function () {
+      return {
+        disable_cookies_reset: false
+      }
+    },
+
     props: ['triggers', 'service_fields', 'component_preloader', 'showSnackbar'],
 
     methods: {
@@ -249,6 +255,7 @@
       },
 
       resetCookies: function () {
+        this.disable_cookies_reset = true;
         this.$http
             .get('/api/internal/v1/popup_config/update_version')
             .then(
@@ -258,7 +265,7 @@
                 err => {
                   console.log(err)
                 }
-            )
+            ).then(() => { this.disable_cookies_reset = false })
       }
     }
   }
@@ -268,5 +275,13 @@
   .after-switch-width {
     max-width: 90px !important;
     min-width: 90px !important;
+  }
+
+  button.theme--light {
+    color: #fff !important;
+  }
+
+  button.theme--light[disabled="disabled"] {
+   color: rgba(0, 0, 0, .26) !important;
   }
 </style>
