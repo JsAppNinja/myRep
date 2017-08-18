c9a8d974eebbe4a98393b08f8208df8910c72944
Linch <andrei95-1995@mail.ru>
2017-08-18 17:14:29 +0300

    added transitions and skeleton pricing page

---
+++ app/javascript/packs/settings/pricing.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-layout row-sm child-flex-sm>
+      <v-flex sm4 offset-sm-2>
+        <v-card>
+          <v-card-title>
+            <h6>HAHAHHAHAH</h6>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+      <v-flex sm4>
+        <v-card>
+          <v-card-title>
+            <h6>HAHAHHAHAH</h6>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
