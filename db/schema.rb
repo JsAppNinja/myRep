0425bfa6f1c5423337267811b3ffcc6ba006f621
Linch <andrei95-1995@mail.ru>
2017-08-18 11:52:26 +0300

    removed devise's email validation and uniqness of email field index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816132538) do
+ActiveRecord::Schema.define(version: 20170818083440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 20170816132538) do
     t.inet "last_sign_in_ip"
     t.string "shopify_script_tag_id"
     t.boolean "enabled", default: false
-    t.index ["email"], name: "index_shops_on_email", unique: true
+    t.index ["email"], name: "index_shops_on_email"
     t.index ["reset_password_token"], name: "index_shops_on_reset_password_token", unique: true
     t.index ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true
   end
